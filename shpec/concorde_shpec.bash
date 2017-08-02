ef151c97d57c4d6f9f57a076d1cf5385bb0522b3
Ted Lilley <ted.lilley@accelerated.com>
2017-08-02 15:51:07 -0400

    bring

--- shpec/concorde_shpec.bash
+++ shpec/concorde_shpec.bash
@@ -47,7 +47,9 @@ describe bring
     $(grab root from_feature concorde)
     temp=$root/lib/temp.bash
     echo $'one () { :;}\ntwo () { :;}' >"$temp"
+    set -x
     $(bring '( one two )' from "$temp")
+    set +x
     assert equal $'one\ntwo' "$(declare -F one two)"
     rm "$temp"
     return "$_shpec_failures" ); : $(( _shpec_failures+= $? ))
