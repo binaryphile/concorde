1b3d8a67f484f4707c658f2102c718f288d7414b
Ted Lilley <ted.lilley@accelerated.com>
2017-09-10 02:10:46 -0400

    grab

--- lib/concorde.bash
+++ lib/concorde.bash
@@ -29,7 +29,7 @@ assign () {
 }
 
 bring () {
-  [[ $2 == 'from' ]] || return
+  [[ $2 == from ]] || return
   is_literal "$1" && eval "local -a function_ary=$1" || local -a function_ary=( $1 )
   local spec=$3
   local feature
