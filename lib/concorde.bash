becbe39c92a3f6eca2be7410a1e067ae668ff758
Ted Lilley <ted.lilley@gmail.com>
2017-10-22 04:16:54 +0000

    emit

--- lib/concorde.bash
+++ lib/concorde.bash
@@ -1,3 +1,9 @@
+concorde.emit () {
+  concorde.xtrace_begin
+  printf 'eval eval %q' "$1"
+  concorde.xtrace_end
+}
+
 concorde.xtrace_begin () {
   (( ${__xtrace:-} )) && return;:
   [[ $- != *x* ]] && __xtrace_set=$? || __xtrace_set=$?
