4f5c5dac4411b029bbff0d36865879f54d27ea9c
Ted Lilley <ted.lilley@gmail.com>
2017-10-22 04:27:45 +0000

    raise

--- lib/concorde.bash
+++ lib/concorde.bash
@@ -23,6 +23,39 @@ concorde.get_raw () {
   IFS=$'\n' read -rd '' __ ||:
 }
 
+concorde.raise () {
+  local rc=$?
+  concorde.xtrace_begin
+  local type=''
+  local msg=''
+  local return=1
+
+  [[ -n ${1:-} && $1 != rc=?* && $1 != return=?* ]] && { type=$1; shift ;}
+  [[ -n ${1:-} && $1 != rc=?* && $1 != return=?* ]] && { msg=$1 ; shift ;}
+  while (( $# )); do
+    case $1 in
+      rc=*      ) rc=${1#rc=}         ;;
+      return=*  ) return=${1#return=} ;;
+    esac
+    shift
+  done
+  [[ $rc == 113 && -z $type ]] && { concorde.emit 'return 113'; return ;}
+  [[ -z $type ]] && type=StandardError
+  concorde.get <<'  EOS'
+    __errcode=%q
+    __errmsg=%q
+    __errtype=%q
+  EOS
+  __+=$'\n'
+  case $return in
+    0 ) __+='( exit 113 )';;
+    * ) __+='return 113'  ;;
+  esac
+  printf -v __ "$__" "$rc" "$msg" "$type"
+  concorde.emit "$__"
+  concorde.xtrace_end
+}
+
 concorde.xtrace_begin () {
   (( ${__xtrace:-} )) && return;:
   [[ $- != *x* ]] && __xtrace_set=$? || __xtrace_set=$?
