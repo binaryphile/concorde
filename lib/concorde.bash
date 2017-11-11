ad35bba79d38bcabb05e7e3faa86f92c19b717a6
Ted Lilley <ted.lilley@gmail.com>
2017-11-11 21:44:15 +0000

    xtrace_begin

---
+++ lib/concorde.bash
@@ -0,0 +1,5 @@
+concorde.xtrace_begin () {
+  (( ${__xtrace:-} )) && return;:
+  [[ $- != *x* ]] && __xtrace_set=$? || __xtrace_set=$?
+  set +o xtrace
+}
--- shpec/concorde_shpec.bash
+++ shpec/concorde_shpec.bash
@@ -4,3 +4,20 @@ mkdir --parents -- "$TMPDIR"
 set -o nounset
 
 source "$(dirname -- "$(readlink --canonicalize -- "$BASH_SOURCE")")"/../lib/concorde.bash
+
+describe concorde.xtrace_begin
+  it "turns off trace if __xtrace is not set"; ( _shpec_failures=0
+    stub_command set 'echo "$@"'
+
+    assert equal '+o xtrace' "$(concorde.xtrace_begin)"
+    return "$_shpec_failures" );: $(( _shpec_failures += $?))
+  end
+
+  it "doesn't turns off trace if __xtrace is set"; ( _shpec_failures=0
+    stub_command set 'echo "$@"'
+
+    __xtrace=1
+    assert equal '' "$(concorde.xtrace_begin)"
+    return "$_shpec_failures" );: $(( _shpec_failures += $?))
+  end
+end
