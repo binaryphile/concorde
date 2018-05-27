5326dfc255ed44209819d46faa802d6dffb08208
Ted Lilley <ted.lilley@gmail.com>
2018-05-27 21:54:04 +0000

    emit

--- shpec/concorde_shpec.bash
+++ shpec/concorde_shpec.bash
@@ -5,6 +5,34 @@ mkdir --parents -- "$TMPDIR"
 
 source "$(dirname -- "$(readlink --canonicalize -- "$BASH_SOURCE")")"/../lib/concorde.bash
 
+describe concorde.emit
+  it "echos hello"; ( _shpec_failures=0
+    assert equal hello "$( $(concorde.emit 'echo hello') )"
+    return "$_shpec_failures" ); (( _shpec_failures += $? ))
+  end
+
+  it "executes a compound statement"; ( _shpec_failures=0
+    assert equal $'hello\nthere' "$( $(concorde.emit 'echo hello; echo there') )"
+    return "$_shpec_failures" ); (( _shpec_failures += $? ))
+  end
+
+  it "executes a multiline statement"; ( _shpec_failures=0
+    assert equal $'hello\nthere' "$( $(concorde.emit $'echo hello\necho there') )"
+    return "$_shpec_failures" ); (( _shpec_failures += $? ))
+  end
+
+  it "declares a variable"; ( _shpec_failures=0
+    $(concorde.emit 'declare sample=example')
+    assert equal example "$sample"
+    return "$_shpec_failures" ); (( _shpec_failures += $? ))
+  end
+
+  it "returns"; ( _shpec_failures=0
+    assert equal '' "$($(concorde.emit return); echo hello)"
+    return "$_shpec_failures" ); (( _shpec_failures += $? ))
+  end
+end
+
 describe concorde.xtrace_begin
   it "turns off trace if __xtrace is not set"; ( _shpec_failures=0
     stub_command set 'echo "$@"'
