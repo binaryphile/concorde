a5f034579cf1e01598dbb581f6de35b049d7e90c
Ted Lilley <ted.lilley@gmail.com>
2018-05-27 22:44:44 +0000

    raise

--- lib/concorde.bash
+++ lib/concorde.bash
@@ -1,5 +1,47 @@
+__code=113
+
 concorde.emit () {
   printf 'eval eval %q' "$1"
+}
+
+raise () {
+  local rc=$?
+  concorde.xtrace_begin
+  local type=''
+  local msg=''
+  local return=1
+
+  [[ -n ${1:-} && $1 != rc=?* && $1 != return=?* ]] && { type=$1; shift ;}
+  [[ -n ${1:-} && $1 != rc=?* && $1 != return=?* ]] && { msg=$1 ; shift ;}
+  while (( $# )); do
+    case $1 in
+      rc=*      ) rc=${1#rc=}         ;;
+      return=*  ) return=${1#return=} ;;
+    esac
+    shift
+  done
+  [[ -z $type && $return != 0 ]] && {
+    [[ -n $__errtype ]] && {
+      concorde.emit "return $__code"
+      return
+    }
+    type=StandardError
+  }
+  __=$'
+    __errcode=%q
+    __errmsg=%q
+    __errtype=%q\n
+  '
+  case $return in
+    0 ) __+="(exit $__code)";;
+    * ) __+="return $__code";;
+  esac
+  case $type in
+    ''  ) printf -v __ "$__" "$__errcode" "$__errmsg" "$__errtype";;
+    *   ) printf -v __ "$__" "$rc"        "$msg"      "$type"     ;;
+  esac
+  concorde.emit "$__"
+  concorde.xtrace_end
 }
 
 concorde.xtrace_begin () {
--- shpec/concorde_shpec.bash
+++ shpec/concorde_shpec.bash
@@ -36,6 +36,76 @@ describe concorde.emit
   end
 end
 
+describe raise
+  it "returns"; ( _shpec_failures=0
+    samplef () { $(raise SampleError); $_echo hello ;}
+    result=$(samplef)
+    assert equal '' "$result"
+    return "$_shpec_failures" ); (( _shpec_failures+=$? ))
+  end
+
+  it "has a result code of 113"; ( _shpec_failures=0
+    samplef () { $(raise SampleError) ;}
+    samplef
+    assert equal 113 $?
+    return "$_shpec_failures" ); (( _shpec_failures+=$? ))
+  end
+
+  it "stores the result code of the last command by default"; ( _shpec_failures=0
+    samplef () { ( exit 123 ); $(raise SampleError) ;}
+    samplef
+    assert equal 123 "$__errcode"
+    return "$_shpec_failures" ); (( _shpec_failures+=$? ))
+  end
+
+  it "stores a blank error message by default"; ( _shpec_failures=0
+    __errmsg=sample
+    samplef () { $(raise SampleError) ;}
+    samplef
+    assert equal '' "$__errmsg"
+    return "$_shpec_failures" ); (( _shpec_failures+=$? ))
+  end
+
+  it "allows no return to be specified"; ( _shpec_failures=0
+    samplef () { raise SampleError return=0 ;}
+    $(samplef)
+    assert equal '(113) (SampleError)' "($?) ($__errtype)"
+    return "$_shpec_failures" ); (( _shpec_failures+=$? ))
+  end
+
+  it "allows the error code to be specified"; ( _shpec_failures=0
+    samplef () { ( exit 123 ); $(raise SampleError rc=222) ;}
+    samplef
+    assert equal 222 "$__errcode"
+    return "$_shpec_failures" ); (( _shpec_failures+=$? ))
+  end
+
+  it "reraises an exception"; ( _shpec_failures=0
+    __code=0
+    samplef   () { $(raise SampleError "a sample error" rc=1) ;}
+    samplef2  () { samplef; $(raise)                          ;}
+    samplef2
+    assert equal '(0) (1) (SampleError) (a sample error)' "($?) ($__errcode) ($__errtype) ($__errmsg)"
+    return "$_shpec_failures" ); (( _shpec_failures+=$? ))
+  end
+
+  it "allows no return on a reraises"; ( _shpec_failures=0
+    __code=0
+    samplef   () { $(raise SampleError "a sample error" rc=1) ;}
+    samplef2  () { samplef; raise return=0                    ;}
+    $(samplef2)
+    assert equal '(0) (1) (SampleError) (a sample error)' "($?) ($__errcode) ($__errtype) ($__errmsg)"
+    return "$_shpec_failures" ); (( _shpec_failures+=$? ))
+  end
+
+  it "raises a standarderror by default"; ( _shpec_failures=0
+    samplef () { $(raise) ;}
+    samplef
+    assert equal '(113) (0) (StandardError) ()' "($?) ($__errcode) ($__errtype) ($__errmsg)"
+    return "$_shpec_failures" ); (( _shpec_failures+=$? ))
+  end
+end
+
 describe concorde.xtrace_begin
   it "turns off trace if __xtrace is not set"; ( _shpec_failures=0
     stub_command set '$_echo "$*"'
