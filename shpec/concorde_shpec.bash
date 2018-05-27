6f562a84aaa92a91dfc47ce4c7926087157525b9
Ted Lilley <ted.lilley@gmail.com>
2018-05-27 22:34:52 +0000

    xtrace

--- shpec/concorde_shpec.bash
+++ shpec/concorde_shpec.bash
@@ -1,6 +1,40 @@
 export TMPDIR=${TMPDIR:-$HOME/tmp}
 mkdir --parents -- "$TMPDIR"
 
-set -o nounset
+! true && set -o nounset
 
 source "$(dirname -- "$(readlink --canonicalize -- "$BASH_SOURCE")")"/../lib/concorde.bash
+
+describe concorde.xtrace_begin
+  it "turns off trace if __xtrace is not set"; ( _shpec_failures=0
+    stub_command set 'echo "$@"'
+
+    assert equal '+o xtrace' "$(concorde.xtrace_begin)"
+    return "$_shpec_failures" ); (( _shpec_failures += $? ))
+  end
+
+  it "doesn't turns off trace if __xtrace is set"; ( _shpec_failures=0
+    stub_command set 'echo "$@"'
+
+    __xtrace=1
+    assert equal '' "$(concorde.xtrace_begin)"
+    return "$_shpec_failures" ); (( _shpec_failures += $? ))
+  end
+end
+
+describe concorde.xtrace_end
+  it "doesn't turn on trace if __xtrace_set is not set"; ( _shpec_failures=0
+    stub_command set 'echo "$@"'
+
+    assert equal '' "$(concorde.xtrace_end)"
+    return "$_shpec_failures" ); (( _shpec_failures += $? ))
+  end
+
+  it "turns on trace if __xtrace_set is set"; ( _shpec_failures=0
+    stub_command set 'echo "$@"'
+
+    __xtrace_set=1
+    assert equal '-o xtrace' "$(concorde.xtrace_end)"
+    return "$_shpec_failures" ); (( _shpec_failures += $? ))
+  end
+end
