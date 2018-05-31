0bba26d3a104292b8d520c779f9de97dee4163e6
Ted Lilley <ted.lilley@gmail.com>
2018-05-31 23:24:51 +0000

    commands

--- shpec/concorde_shpec.bash
+++ shpec/concorde_shpec.bash
@@ -3,7 +3,13 @@ mkdir --parents -- "$TMPDIR"
 
 ! true && set -o nounset
 
-source "$(dirname -- "$(readlink --canonicalize -- "$BASH_SOURCE")")"/../lib/concorde.bash
+_echo='printf %s\n'
+_mkdir='mkdir --parents --'
+_mktempd='mktemp --directory --quiet'
+_rmtree='rm --recursive --force --'
+_source_dir=$(dirname -- "$(readlink --canonicalize -- "$BASH_SOURCE")")
+
+source "$_source_dir"/../lib/concorde.bash
 
 describe concorde
   it "is a module"; ( _shpec_failures=0
@@ -99,17 +105,17 @@ end
 
 describe concorde.emit
   it "echos hello"; ( _shpec_failures=0
-    assert equal hello "$( $(concorde.emit 'echo hello') )"
+    assert equal hello "$( $(concorde.emit '$_echo hello') )"
     return "$_shpec_failures" ); (( _shpec_failures += $? ))
   end
 
   it "executes a compound statement"; ( _shpec_failures=0
-    assert equal $'hello\nthere' "$( $(concorde.emit 'echo hello; echo there') )"
+    assert equal $'hello\nthere' "$( $(concorde.emit '$_echo hello; $_echo there') )"
     return "$_shpec_failures" ); (( _shpec_failures += $? ))
   end
 
   it "executes a multiline statement"; ( _shpec_failures=0
-    assert equal $'hello\nthere' "$( $(concorde.emit $'echo hello\necho there') )"
+    assert equal $'hello\nthere' "$( $(concorde.emit $'$_echo hello\n$_echo there') )"
     return "$_shpec_failures" ); (( _shpec_failures += $? ))
   end
 
@@ -120,7 +126,7 @@ describe concorde.emit
   end
 
   it "returns"; ( _shpec_failures=0
-    assert equal '' "$($(concorde.emit return); echo hello)"
+    assert equal '' "$($(concorde.emit return); $_echo hello)"
     return "$_shpec_failures" ); (( _shpec_failures += $? ))
   end
 end
@@ -160,7 +166,7 @@ describe concorde.module
 
   it "doesn't reload"; ( _shpec_failures=0
     $(concorde.module sample)
-    result=$($(concorde.module sample); echo hello)
+    result=$($(concorde.module sample); $_echo hello)
     assert equal '(0) ()' "($?) ($result)"
     return "$_shpec_failures" ); (( _shpec_failures += $? ))
   end
@@ -168,7 +174,7 @@ describe concorde.module
   it "reloads if the last argument is reload=1"; ( _shpec_failures=0
     set -- one two reload=1
     $(concorde.module sample)
-    result=$($(concorde.module sample); echo hello)
+    result=$($(concorde.module sample); $_echo hello)
     assert equal hello "$result"
     return "$_shpec_failures" ); (( _shpec_failures += $? ))
   end
@@ -321,7 +327,7 @@ describe concorde.traceback
   end
 
   it "turns off strict mode"; ( _shpec_failures=0
-    stub_command concorde.strict_mode 'echo "$@"'
+    stub_command concorde.strict_mode '$_echo "$@"'
 
     assert equal off "$(concorde.traceback 2>/dev/null)"
     return "$_shpec_failures" ); (( _shpec_failures+=$? ))
@@ -367,14 +373,14 @@ end
 
 describe concorde.xtrace_begin
   it "turns off trace if __xtrace is not set"; ( _shpec_failures=0
-    stub_command set 'echo "$@"'
+    stub_command set '$_echo "$*"'
 
     assert equal '+o xtrace' "$(concorde.xtrace_begin)"
     return "$_shpec_failures" ); (( _shpec_failures += $? ))
   end
 
   it "doesn't turns off trace if __xtrace is set"; ( _shpec_failures=0
-    stub_command set 'echo "$@"'
+    stub_command set '$_echo "$*"'
 
     __xtrace=1
     assert equal '' "$(concorde.xtrace_begin)"
@@ -384,14 +390,14 @@ end
 
 describe concorde.xtrace_end
   it "doesn't turn on trace if __xtrace_set is not set"; ( _shpec_failures=0
-    stub_command set 'echo "$@"'
+    stub_command set '$_echo "$*"'
 
     assert equal '' "$(concorde.xtrace_end)"
     return "$_shpec_failures" ); (( _shpec_failures += $? ))
   end
 
   it "turns on trace if __xtrace_set is set"; ( _shpec_failures=0
-    stub_command set 'echo "$@"'
+    stub_command set '$_echo "$*"'
 
     __xtrace_set=1
     assert equal '-o xtrace' "$(concorde.xtrace_end)"
