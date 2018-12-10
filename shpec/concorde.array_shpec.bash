5f6e67cc44821b5503ed195315b87e04a053ea33
Ted Lilley <ted.lilley@gmail.com>
2018-12-10 02:33:03 +0000

    test

--- shpec/concorde.array_shpec.bash
+++ shpec/concorde.array_shpec.bash
@@ -3,36 +3,32 @@ set -o noglob
 
 shpec_Dir=$(dirname $(readlink -f $BASH_SOURCE))/..
 source $shpec_Dir/shpec/shpec-helper.bash
-source $shpec_Dir/lib/as module a=$shpec_Dir/lib/concorde.array.bash
+source $shpec_Dir/lib/concorde.bash
 
 describe all
   it "returns true if none of the elements return false"
-    source $shpec_Dir/lib/as module s=$shpec_Dir/lib/concorde.bash
     samples=( zero one two )
-    a.all samples s.present?
+    ary.all samples present?
     assert equal 0 $?
   ti
 
   it "returns false if any of the elements return false"
-    source $shpec_Dir/lib/as module s=$shpec_Dir/lib/concorde.bash
     samples=( zero one two )
-    ! a.all samples s.blank?
+    ! ary.all samples blank?
     assert equal 0 $?
   ti
 end_describe
 
 describe any
   it "returns true if one of the elements returns true"
-    source $shpec_Dir/lib/as module s=$shpec_Dir/lib/concorde.bash
     samples=( zero one '' )
-    a.any samples s.blank?
+    ary.any samples blank?
     assert equal 0 $?
   ti
 
   it "returns false if all of the elements return false"
-    source $shpec_Dir/lib/as module s=$shpec_Dir/lib/concorde.bash
     samples=( zero one two )
-    ! a.any samples s.blank?
+    ! ary.any samples blank?
     assert equal 0 $?
   ti
 end_describe
@@ -40,13 +36,13 @@ end_describe
 describe join
   it "joins strings with no delimiter"
     samples=( a b c )
-    a.join result samples ''
+    ary.join result samples ''
     assert equal abc $result
   ti
 
   it "joins strings with a multicharacter delimiter"
     samples=( a b c )
-    a.join result samples --
+    ary.join result samples --
     assert equal a--b--c $result
   ti
 end_describe
