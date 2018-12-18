2d9f425843b3db41f25af23005238da333b75802
Ted Lilley <ted.lilley@gmail.com>
2018-12-18 02:55:00 +0000

    return_

--- lib/concorde.bash
+++ lib/concorde.bash
@@ -1,8 +1,9 @@
 concorde_Dir=$(dirname $(readlink -f $BASH_SOURCE))
 
-source $concorde_Dir/as module            \
-  ary=$concorde_Dir/concorde.array.bash   \
-  int=$concorde_Dir/concorde.integer.bash
+source $concorde_Dir/as module          \
+  s=$concorde_Dir/concorde.string.bash  \
+  a=$concorde_Dir/concorde.array.bash   \
+  i=$concorde_Dir/concorde.integer.bash
 
 shopt -s expand_aliases
 alias kwargs='(( $# )) && declare'
@@ -11,7 +12,7 @@ alias_var () {
   local item
 
   for item; do
-    eval "alias $item='_var_helper_ $item'"
+    eval "alias $item='var_ $item'"
   done
 }
 
@@ -28,10 +29,6 @@ array () {
   echo "${results_[*]}"
 }
 
-ascii_only?  () {
-  [[ ${1:-} != *[^[:ascii:]]* ]]
-}
-
 blank? () {
   local item=${1:-}
 
@@ -43,127 +40,6 @@ capitalize () {
 
   ref_=${2,,}
   ref_=${ref_^}
-}
-
-alias_var pad_
-center () {
-  local -n ref_=$1
-  local length_=${#2}
-  local width_=$3
-  local padstr_=${4:- }
-  local -i num_
-  local pad_
-
-  num_="(width_-length_)/(2*${#padstr_})"
-  pad_ = int.times $num_ $padstr_
-  num_=num_%${#padstr_}
-  pad_+=${padstr_:0:num_-1}
-  ref_=$pad_$2
-  num_="(width_-(${#pad_}+length_))/${#padstr_}"
-  pad_ = int.times $num_ $padstr_
-  num_=num_%${#padstr_}
-  pad_+=${padstr_:0:num_}
-  ref_+=$pad_
-}
-unalias pad_
-
-chars () {
-  local -n ref_=$1
-  local i_
-
-  for (( i_ = 0; i_ < ${#2}; i_++ )); do
-    ref_+=( ${2:i_:1} )
-  done
-}
-
-chomp () {
-  local -n ref_=$1
-
-  case $# in
-    2 )
-      case $2 in
-        *$'\r\n'      ) ref_=${2%$'\r\n'} ;;
-        *$'\r'|*$'\n' ) ref_=${2%?}       ;;
-        *             ) ref_=$2           ;;
-      esac
-      ;;
-    3 )
-      ref_=$2
-      case $3 in
-        '' )
-          while [[ $ref_ == *$'\r\n' ]]; do
-            ref_=${ref_%$'\r\n'}
-          done;:
-          ;;
-        * ) ref_=${2%$3}
-      esac
-      ;;
-  esac
-}
-
-chop () {
-  case $2 in
-    *$'\r\n'  ) printf -v $1 %s "${2%$'\r\n'}";;
-    *         ) printf -v $1 %s "${2%?}"      ;;
-  esac
-}
-
-chr () {
-  printf -v $1 %s ${2:0:1}
-}
-
-codepoints () {
-  local i_
-
-  for (( i_ = 0; i_ < ${#1}; i_++ )); do
-    printf -v $1[i_] %d "'${2:i_:1}"
-  done
-}
-
-compare () {
-  local -n ref_=$1
-
-  [[ $2 < $3    ]] && ref_=-1
-  [[ $2 == "$3" ]] && ref_=0
-  [[ $2 > $3    ]] && ref_=1;:
-}
-
-count () {
-  local -n ref_=$1
-  local target_=$2
-  local spec_
-  local result_
-
-  for spec_ in ${*:3:$#-2}; do
-    for (( i_ = 0; i_ < ${#target_}; i_++ )); do
-      [[ ${target_:i_:1} == [$spec_] ]] && result_+=${target_:i_:1}
-    done
-    target_=$result_
-    result_=''
-  done
-  ref_=${#target_}
-}
-
-delete () {
-  local -n ref_=$1
-  local target_=$2
-  local copy_=$2
-  local result_
-  local spec_
-
-  for spec_ in ${*:3:$#-2}; do
-    for (( i_ = 0; i_ < ${#target_}; i_++ )); do
-      [[ ${target_:i_:1} == [$spec_] ]] && result_+=${target_:i_:1}
-    done
-    target_=$result_
-    result_=''
-  done
-
-  target_=${target_//^/\^}
-  target_=${target_//-/\-}
-  for (( i_ = 0; i_ < ${#copy_}; i_++ )); do
-    [[ ${copy_:i_:1} != [$target_] ]] && ref_+=${copy_:i_:1};:
-  done
 }
 
 downcase () {
@@ -188,74 +64,12 @@ end_with? () {
   return 1
 }
 
-eq? () {
-  [[ $1 == "$2" ]]
-}
-
-ge? () {
-  [[ $1 > $2 || $1 == "$2" ]]
-}
-
-getbyte () {
-  printf -v $1 %d \'${2:$3:1}
-}
-
 gsub () {
   printf -v $1 %s ${2//$3/$4}
 }
 
-gt? () {
-  [[ $1 > $2 ]]
-}
-
-hex () {
-  [[ $2 == *[^+\-[:digit:]a-fx]* ]] && {
-    printf -v $1 %s 0
-    return
-  }
-  case $2 in
-    0x*|0X* ) printf -v $1 %d $2        ;;
-    -*      ) printf -v $1 %d -0x${2#-} ;;
-    *       ) printf -v $1 %d 0x$2      ;;
-  esac
-}
-
 include? () {
   [[ $1 == *"$2"* ]]
-}
-
-index () {
-  local -n ref_=$1
-  local target_=$2
-  local search_=$3
-  local i_
-  local offset=${4:-0}
-
-  ref_=''
-  for (( i_ = $offset; i_ < ${#target_} - ${#search_} + 1; i_++ )); do
-    eq? ${target_:i_:${#search_}} $search_ && {
-      ref_=$i_
-      return
-    };:
-  done
-}
-
-insert () {
-  case $3 in
-    -1  ) printf -v $1 %s ${2:0:$#+$3+1}$4          ;;
-    -*  ) printf -v $1 %s ${2:0:$#+$3+1}$4${2:$3+1} ;;
-    *   ) printf -v $1 %s ${2:0:$3}$4${2:$3}        ;;
-  esac
-}
-
-inspect () {
-  printf -v $1 %q $2
-  [[ ${!1} == \$\'*\' ]] && return
-  printf -v $1 '"%s"' ${!1}
-}
-
-le? () {
-  [[ $1 < $2 || $1 == "$2" ]]
 }
 
 left () {
@@ -274,78 +88,24 @@ lines () {
   ref_=( $2 )
 }
 
-alias_var pad_
-ljust () {
-  local -n ref_=$1
-  local padstr_=${4:- }
-  local -i mod_
-  local -i num_
-
-  num_=($3-${#2})/${#padstr_}
-  mod_=($3-${#2})%${#padstr_}
-  pad_ = int.times $num_ $padstr_
-  ref_=$2$pad_${padstr_:0:mod_}
-}
-unalias pad_
-
-lstrip () {
-  local -n ref_=$1
-
-  ref_=${2%%[^[:space:]]*}
-  ref_=${2#$ref_}
-}
-
-lt? () {
-  [[ $1 < $2 ]]
-}
-
-next () {
-  local -n ref_=$1
-  local -i ord_
-  local IFS=$IFS
-  local chr_
-  local i_
-  local results_=()
-
-  carry_=1
-  for (( i_ = ${#2}; i_ > 0; i_-- )); do
-    chr_=${2:i_-1:1}
-    ! (( carry_ )) && {
-      results_[i_]=$chr_
-      continue
-    }
-    printf -v ord_ %d \'$chr_
-    ord_+=1
-    printf -v ord_ %02o $ord_
-    case $chr_ in
-      [a-y]|[A-Y]|[0-8] ) printf -v results_[i_] %b \\$ord_; carry_=0   ;;
-      z|Z               ) printf -v results_[i_] %b \\$(( ord_ - 32 ))  ;;
-      9                 ) results_[i_]=0                                ;;
-      *                 ) results_[i_]=$chr_                            ;;
-    esac
-  done
-  (( carry_ )) && case $chr_ in
-    z|Z ) printf -v results_[0] %b \\$(( ord_ - 32 )) ;;
-    9   ) results_[0]=1                               ;;
-  esac
-  IFS=''
-  ref_=${results_[*]}
-}
-
 ord () {
   printf -v $1 %d \'$2
-}
-
-partition () {
-  local -n ref_=$1
-
-  ref_=( ${2%%$3*} $3 ${2#*$3} )
 }
 
 present? () {
   local item=${1:-}
 
   [[ -n ${item//[[:space:]]} ]]
+}
+
+return_ () {
+  local result_
+
+  result_=$(declare -p $2)
+  result_=${result_#*=}
+  result_=${result_#\'}
+  unset -v $1
+  eval $1=${result_%\'}
 }
 
 reverse () {
@@ -361,80 +121,8 @@ right () {
   printf -v $1 %s ${2:${#2}-$3:${#2}}
 }
 
-rindex () {
-  local -n ref_=$1
-  local target_=$2
-  local search_=$3
-  local -i offset=${4:-${#target_}}
-  local i_
-
-  ref_=''
-  for (( i_ = $offset - ${#search_}; i_ >= 0; i_-- )); do
-    eq? ${target_:i_:${#search_}} $search_ && {
-      ref_=$i_
-      return
-    };:
-  done
-}
-
-alias_var pad_
-rjust () {
-  local -n ref_=$1
-  local padstr_=${4:- }
-  local -i mod_
-  local -i num_
-
-  num_="($3-${#2})/${#padstr_}"
-  mod_="($3-${#2})%${#padstr_}"
-  pad_ = int.times $num_ $padstr_
-  ref_=$pad_${padstr_:0:mod_}$2
-}
-unalias pad_
-
-rpartition () {
-  local -n ref_=$1
-
-  ref_=( ${2%$3*} $3 ${2##*$3} )
-}
-
-rstrip () {
-  local -n ref_=$1
-
-  ref_=${2##*[^[:space:]]}
-  ref_=${2%$ref_}
-}
-
-scan () {
-  local -n ref_=$1
-  local target_=$2
-  local pattern_=${3#/}
-
-  pattern_=${pattern_%/}
-  ref_=()
-  while [[ $target_ =~ $pattern_ ]]; do
-    ref_+=( $BASH_REMATCH )
-    target_=${target_#*$BASH_REMATCH}
-  done
-}
-
 slice () {
   printf -v $1 %s ${2:$3:${4:-1}}
-}
-
-split () {
-  local -n ref_=$1; shift
-  local delim_=${2:-[[:space:]]}
-
-  while [[ $1 != ${1/$delim_} ]]; do
-    ref_+=( ${1%%$delim_*} )
-    set -- "${1#*$delim_}"
-  done
-  ref_+=( $1 )
-}
-
-strip () {
-  lstrip __ $2
-  rstrip $1 $__
 }
 
 substr () {
@@ -445,7 +133,7 @@ upcase () {
   printf -v $1 ${2^^}
 }
 
-_var_helper_ () {
+var_ () {
   [[ $3 == printf ]] && {
     printf -v $1 ${*:3}
     return
--- shpec/concorde_shpec.bash
+++ shpec/concorde_shpec.bash
@@ -56,19 +56,6 @@ describe array
   ti
 end_describe
 
-describe ascii_only?
-  it "is true if only ascii characters"
-    ascii_only? abc
-    assert equal 0 $?
-  ti
-
-  it "is false if there are non-ascii characters"
-    printf -v sample '\xE2\x98\xA0'
-    ! ascii_only? $sample
-    assert equal 0 $?
-  ti
-end_describe
-
 describe blank?
   it "is true if no argument"
     blank?
@@ -95,192 +82,6 @@ describe capitalize
   it "capitalizes a word"
     capitalize result HELLO
     assert equal Hello $result
-  ti
-end_describe
-
-describe center
-  it "doesn't extend a too short width"
-    center result hello 4
-    assert equal hello $result
-  ti
-
-  it "centers in a wider area"
-    center result hello 20
-    assert equal '       hello        ' $result
-  ti
-
-  it "centers with a padstr"
-    center result hello 20 123
-    assert equal '1231231hello12312312' $result
-  ti
-end_describe
-
-describe chars
-  it "converts a string to an array of characters"
-    chars results abc
-    expecteds=( a b c )
-    assert equal "${expecteds[*]}" "${results[*]}"
-  ti
-end_describe
-
-describe chomp
-  it "doesn't chomp nothing"
-    chomp result hello
-    assert equal hello "$result"
-  ti
-
-  it "chomps newline"
-    chomp result $'hello\n'
-    assert equal hello "$result"
-  ti
-
-  it "chomps carriage-return newline"
-    chomp result $'hello\r\n'
-    assert equal hello "$result"
-  ti
-
-  it "only chomps one qualifying trailer"
-    chomp result $'hello\n\r'
-    assert equal $'hello\n' "$result"
-  ti
-
-  it "chomps a carriage-return"
-    chomp result $'hello\r'
-    assert equal hello "$result"
-  ti
-
-  it "doesn't chomp a non-terminal newline"
-    chomp result $'hello \n there'
-    assert equal $'hello \n there' "$result"
-  ti
-
-  it "chomps a specified trailer"
-    chomp result hello llo
-    assert equal he "$result"
-  ti
-
-  it "removes trailing carriage-return newlines if given an empty string"
-    chomp result $'hello\r\n\r\n' ''
-    assert equal hello "$result"
-  ti
-
-  it "doesn't remove trailing newlines if given an empty string"
-    chomp result $'hello\r\n\r\r\n' ''
-    assert equal $'hello\r\n\r' "$result"
-  ti
-end_describe
-
-describe chop
-  it "chops carriage-return newline"
-    chop result $'string\r\n'
-    assert equal string "$result"
-  ti
-
-  it "chops carriage-return"
-    chop result $'string\n\r'
-    assert equal $'string\n' "$result"
-  ti
-
-  it "chops newline"
-    chop result $'string\n'
-    assert equal string "$result"
-  ti
-
-  it "chops the last letter"
-    chop result string
-    assert equal strin "$result"
-  ti
-
-  it "chops an empty string"
-    chop result x
-    chop result "$result"
-    assert equal '' "$result"
-  ti
-end_describe
-
-describe chr
-  it "returns one character from the beginning of a string"
-    chr result abc
-    assert equal a $result
-  ti
-end_describe
-
-describe codepoints
-  it "returns an array of the integers for the characters"
-    codepoints result $'hello\u0639'
-    expecteds=( 104 101 108 108 111 1593 )
-    assert equal "${expecteds[*]}" "${result[*]}"
-  ti
-end_describe
-
-describe compare
-  it "returns -1 if the string is less than another"
-    compare result a b
-    assert equal -1 $result
-  ti
-
-  it "returns 0 if the string is the same as another"
-    compare result a a
-    assert equal 0 $result
-  ti
-
-  it "returns 1 if the string is greater than another"
-    compare result b a
-    assert equal 1 $result
-  ti
-end_describe
-
-describe count
-  it "counts ls and os"
-    count result "hello world" lo
-    assert equal 5 $result
-  ti
-
-  it "counts the intersection of two strings"
-    count result "hello world" lo o
-    assert equal 2 $result
-  ti
-
-  it "counts negated"
-    count result "hello world" hello ^l
-    assert equal 4 $result
-  ti
-
-  it "counts a range"
-    count result "hello world" ej-m
-    assert equal 4 $result
-  ti
-
-  it "escapes ^"
-    count result "hello^world" '\^aeiou'
-    assert equal 4 $result
-  ti
-
-  it "escapes -"
-    count result "hello-world" 'a\-eo'
-    assert equal 4 $result
-  ti
-end_describe
-
-describe delete
-  it "deletes a couple strings"
-    delete result hello l lo
-    assert equal heo $result
-  ti
-
-  it "deletes a string"
-    delete result hello lo
-    assert equal he $result
-  ti
-
-  it "deletes a negation"
-    delete result ^hello aeiou ^e
-    assert equal ^hell $result
-  ti
-
-  it "deletes a range"
-    delete result he-llo ej-m
-    assert equal h-o $result
   ti
 end_describe
 
@@ -332,158 +133,10 @@ describe end_with?
   ti
 end_describe
 
-describe eq?
-  it "returns true for equal strings"
-    ! eq? sample sample
-    assert unequal 0 $?
-  ti
-
-  it "returns false for unequal strings"
-    ! eq? sample1 sample
-    assert equal 0 $?
-  ti
-end_describe
-
-describe ge?
-  it "returns true for a greater string comparison"
-    ge? b a
-    assert equal 0 $?
-  ti
-
-  it "returns true for an equal string comparison"
-    ge? a a
-    assert equal 0 $?
-  ti
-
-  it "returns false for a lesser string comparison"
-    ! ge? a b
-    assert equal 0 $?
-  ti
-end_describe
-
-describe getbyte
-  it "returns a byte from the indexed string"
-    getbyte result hello 2
-    assert equal 108 $result
-  ti
-end_describe
-
 describe gsub
   it "substitutes all occurrences of a pattern"
     gsub result hello [aeiou] *
     assert equal h*ll* $result
-  ti
-end_describe
-
-describe gt?
-  it "returns true for a greater string comparison"
-    gt? b a
-    assert equal 0 $?
-  ti
-
-  it "returns false for an equal string comparison"
-    ! gt? a a
-    assert equal 0 $?
-  ti
-
-  it "returns false for a lesser string comparison"
-    ! gt? a b
-    assert equal 0 $?
-  ti
-end_describe
-
-describe hex
-  it "returns a hex value"
-    hex result 0x0a
-    assert equal 10 $result
-  ti
-
-  it "parses a sign"
-    hex result -1234
-    assert equal -4660 $result
-  ti
-
-  it "parses 0"
-    hex result 0
-    assert equal 0 $result
-  ti
-
-  it "returns 0 on error"
-    hex result wombat
-    assert equal 0 $result
-  ti
-end_describe
-
-describe include?
-  it "returns true if one string includes the other"
-    include? sample ampl
-    assert equal 0 $?
-  ti
-
-  it "returns false if one string doesn't include the other"
-    ! include? sample blah
-    assert equal 0 $?
-  ti
-end_describe
-
-describe index
-  it "returns the index of 'e' in 'hello'"
-    index result hello e
-    assert equal 1 $result
-  ti
-
-  it "returns the index of 'lo' in 'hello'"
-    index result hello lo
-    assert equal 3 $result
-  ti
-
-  it "doesn't return the index of 'a' in 'hello'"
-    index result hello a
-    assert equal '' "$result"
-  ti
-
-  it "starts at an offset"
-    index result hello l 3
-    assert equal 3 $result
-  ti
-end_describe
-
-describe insert
-  it "inserts before an index"
-    insert result abcd 0 X
-    assert equal Xabcd $result
-  ti
-
-  it "inserts before another index"
-    insert result abcd 3 X
-    assert equal abcXd $result
-  ti
-
-  it "inserts before yet another index"
-    insert result abcd 4 X
-    assert equal abcdX $result
-  ti
-
-  it "inserts before a negative index"
-    insert result abcd -3 X
-    assert equal abXcd $result
-  ti
-
-  it "inserts before another negative index"
-    insert result abcd -1 X
-    assert equal abcdX $result
-  ti
-end_describe
-
-describe inspect
-  it "returns a quoted string"
-    inspect result $'hel\bo'
-    assert equal "$'hel\bo'" $result
-  ti
-
-  it "returns a quoted string"
-    inspect result hello
-    assert equal '"hello"' $result
   ti
 end_describe
 
@@ -495,23 +148,6 @@ describe kwargs
     }
     result=$(samplef sample=text)
     assert equal text $result
-  ti
-end_describe
-
-describe le?
-  it "returns true for a lesser string comparison"
-    le? a b
-    assert equal 0 $?
-  ti
-
-  it "returns true for an equal string comparison"
-    le? a a
-    assert equal 0 $?
-  ti
-
-  it "returns false for a greater string comparison"
-    ! le? b a
-    assert equal 0 $?
   ti
 end_describe
 
@@ -543,89 +179,6 @@ describe lines
   ti
 end_describe
 
-describe ljust
-  it "returns the string if the pad is shorter"
-    ljust result hello 4
-    assert equal hello $result
-  ti
-
-  it "returns a padded string"
-    ljust result hello 20
-    assert equal 'hello               ' $result
-  ti
-
-  it "returns a given pad"
-    ljust result hello 20 1234
-    assert equal hello123412341234123 $result
-  ti
-end_describe
-
-describe lstrip
-  it "strips whitespace from the left of a string"
-    lstrip result " whitespace "
-    assert equal "whitespace " $result
-  ti
-end_describe
-
-describe lt?
-  it "returns true for a lesser string comparison"
-    lt? a b
-    assert equal 0 $?
-  ti
-
-  it "returns false for an equal string comparison"
-    ! lt? a a
-    assert equal 0 $?
-  ti
-
-  it "returns false for a greater string comparison"
-    ! lt? b a
-    assert equal 0 $?
-  ti
-end_describe
-
-describe next
-  it "increments a letter"
-    next result abcd
-    assert equal abce $result
-  ti
-
-  it "increments a number"
-    next result THX1138
-    assert equal THX1139 $result
-  ti
-
-  it "skips non-alnums"
-    next result '<<koala>>'
-    assert equal '<<koalb>>' $result
-  ti
-
-  it "carries on lowercase letters and numbers"
-    next result 1999zzz
-    assert equal 2000aaa $result
-  ti
-
-  it "carries on uppercase letters and numbers"
-    next result ZZZ9999
-    assert equal AAAA0000 $result
-  ti
-end_describe
-
-describe ord
-  it "returns the ordinal of a one-character string"
-    ord result a
-    assert equal 97 $result
-  ti
-end_describe
-
-describe partition
-  it "partitions a string into an array"
-    partition results "Spam eggs spam spam and ham" spam
-    expecteds=( "Spam eggs " spam " spam and ham" )
-    assert equal "${expecteds[*]}" "${results[*]}"
-  ti
-end_describe
-
 describe present?
   it "is false if no argument"
     ! present?
@@ -648,6 +201,51 @@ describe present?
   ti
 end_describe
 
+describe return_
+  it "assigns a value from a named variable in another named variable"
+    sample=text
+    return_ result sample
+    assert equal text $result
+  ti
+
+  it "doesn't let a local mask the return"
+    declare result
+    foo () {
+      local result=''
+      local sample=text
+
+      local $1 && return_ $1 sample
+    }
+    foo result
+    assert equal text $result
+  ti
+
+  it "returns an array"
+    declare results=()
+    foo () {
+      local results=()
+      local samples=( one two three )
+
+      local $1 && return_ $1 samples
+    }
+    foo results
+    expecteds=( one two three )
+    assert equal "${expecteds[*]}" "${results[*]}"
+  ti
+
+  it "returns a hash"
+    declare -A results=()
+    foo () {
+      local -A results=()
+      local -A samples=( [one]=1 [two]=2 [three]=3 )
+
+      local $1 && return_ $1 samples
+    }
+    foo results
+    assert equal "declare -A results='([one]=\"1\" [two]=\"2\" [three]=\"3\" )'" $(declare -p results)
+  ti
+end_describe
+
 describe reverse
   it "reverses a string"
     reverse result stressed
@@ -662,69 +260,6 @@ describe right
   ti
 end_describe
 
-describe rindex
-  it "returns the index of 'e' in 'hello'"
-    rindex result hello e
-    assert equal 1 "$result"
-  ti
-
-  it "returns the index of 'l' in 'hello'"
-    rindex result hello l
-    assert equal 3 "$result"
-  ti
-
-  it "doesn't return the index of 'a' in 'hello'"
-    rindex result hello a
-    assert equal '' "$result"
-  ti
-end_describe
-
-describe rjust
-  it "returns the string if the pad is shorter"
-    rjust result hello 4
-    assert equal hello $result
-  ti
-
-  it "returns a padded string"
-    rjust result hello 20
-    assert equal '               hello' $result
-  ti
-
-  it "returns a given pad"
-    rjust result hello 20 1234
-    assert equal 123412341234123hello $result
-  ti
-end_describe
-
-describe rpartition
-  it "partitions a string into an array"
-    rpartition results "Spam eggs spam spam and ham" spam
-    expecteds=( "Spam eggs spam " spam " and ham" )
-    assert equal "${expecteds[*]}" "${results[*]}"
-  ti
-end_describe
-
-describe rstrip
-  it "strips whitespace from the right of a string"
-    rstrip result " whitespace "
-    assert equal " whitespace" $result
-  ti
-end_describe
-
-describe scan
-  it "scans by word"
-    scan results "cruel world" /[[:alnum:]_]+/
-    expecteds=( cruel world )
-    assert equal "${expecteds[*]}" "${results[*]}"
-  ti
-
-  it "scans letters"
-    scan results "cruel world" /.../
-    expecteds=( cru 'el ' wor )
-    assert equal "${expecteds[*]}" "${results[*]}"
-  ti
-end_describe
-
 describe slice
   it "returns an indexed character"
     slice result "hello there" 1
@@ -734,33 +269,6 @@ describe slice
   it "returns an index and length"
     slice result "hello there" 2 3
     assert equal "llo" "$result"
-  ti
-end_describe
-
-describe split
-  it "splits a string into an array"
-    split results " now's  the time" ''
-    expecteds=( "now's" "the" "time" )
-    assert equal "${expecteds[*]}" "${results[*]}"
-  ti
-
-  it "splits a string into an array on space"
-    split results " now's  the time" ' '
-    expecteds=( "now's" "the" "time" )
-    assert equal "${expecteds[*]}" "${results[*]}"
-  ti
-
-  it "splits on a string"
-    split results "mellow yellow" ello
-    expecteds=( m "w y" w )
-    assert equal "${expecteds[*]}" "${results[*]}"
-  ti
-end_describe
-
-describe strip
-  it "strips whitespace from both sides of a string"
-    strip result " whitespace "
-    assert equal whitespace $result
   ti
 end_describe
 
